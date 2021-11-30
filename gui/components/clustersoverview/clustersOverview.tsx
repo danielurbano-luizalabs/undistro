6b8595fec9f0267f2c0d51c3c5c31bf79eee9db2
Felipe Oliveira <fpo@felipeweb.dev>
2021-11-30 17:19:06 -0300

    404

--- gui/components/clustersoverview/clustersOverview.tsx
+++ gui/components/clustersoverview/clustersOverview.tsx
@@ -1,13 +1,15 @@
+import { useRouter } from "next/router";
 import * as React from "react";
-import classes from "./clustersOverview.module.css";
-import Clustersoverviewrow from "./clustersOverviewRow";
-import { Clusteroverviewfooter } from "./clustersOverviewNavFooter";
-import { Cluster, empty } from "../../lib/cluster";
 import { createRef, useEffect, useState } from "react";
-import { useClusters } from "../workspace/clusterctx";
 import { useResizeDetector } from "react-resize-detector";
+import { Cluster, empty } from "../../lib/cluster";
 import { paginate } from "../../lib/pagination";
+import Page404message from "../page404/page404message";
+import { useClusters } from "../workspace/clusterctx";
+import classes from "./clustersOverview.module.css";
 import Clustersoverviewemptyrow from "./clustersOverviewEmptyRow";
+import { Clusteroverviewfooter } from "./clustersOverviewNavFooter";
+import Clustersoverviewrow from "./clustersOverviewRow";
 
 type Props = {
   clusters?: Cluster[];
@@ -16,6 +18,7 @@ type Props = {
 
 const Clustersoverview = (props: Props) => {
   const rowHeight = 36;
+  const router = useRouter();
   const tableContainerRef = createRef<HTMLDivElement>();
   const tableRef = createRef<HTMLTableElement>() || undefined;
   const pageFooterRef = createRef<HTMLDivElement>();
@@ -26,6 +29,7 @@ const Clustersoverview = (props: Props) => {
     targetRef: tableContainerRef,
   });
   const [qtyPages, setQtyPages] = useState<number>(1);
+  const [isValidPage, setValidPage] = useState<boolean>(true);
   const [pageSize, setPageSize] = useState<number>(0);
   const [initialContainerSize, setInitialContainerSize] = useState<number>(0);
   const changeCheckbox = (checked: boolean) => {
@@ -57,35 +61,33 @@ const Clustersoverview = (props: Props) => {
         pagesCalc();
       }
     }
-  }, [clusters, height, initialContainerSize]);
+  }, [clusters, height, initialContainerSize, qtyPages, isValidPage]);
   let pageNumber = parseInt(props.page);
   const pagesCalc = () => {
-    console.log("container ref height");
-    console.log(height!);
-    console.log("initialContainerSize");
-    console.log(initialContainerSize);
-    let table = tableRef?.current;
-    let tableLength = table?.rows.length;
-    console.log("length");
-    console.log(tableLength);
     let pageFooter = pageFooterRef?.current;
     let pageFooterHeight = pageFooter?.getBoundingClientRect().height;
     let pageQtyItems = Math.trunc((height! - pageFooterHeight!) / rowHeight);
     pageQtyItems = pageQtyItems - 1; // ignore table header
-    console.log("pageSize");
-    console.log(pageQtyItems);
     setPageSize(pageQtyItems);
-    let qtyPages = Math.ceil(props.clusters?.length! / pageQtyItems);
-    console.log("pages");
-    console.log(qtyPages);
+
+    if (pageQtyItems > 0) {
+      let qtyPages = Math.ceil(props.clusters?.length! / pageQtyItems);
+      setQtyPages(qtyPages);
+    }
+    if (qtyPages && pageNumber > qtyPages) {
+      console.log("if");
+      console.log(pageNumber);
+      console.log(qtyPages);
+      setValidPage(false);
+    } else {
+      console.log("else");
+      console.log(pageNumber);
+      console.log(qtyPages);
+      setValidPage(true);
+    }
     let pageLists = paginate(props.clusters!, pageQtyItems);
-    console.log("pages List");
-    console.log(pageLists);
-    console.log("list page");
-    console.log(pageLists[pageNumber - 1]);
     if (pageLists.length >= pageNumber) {
       let items = pageLists[pageNumber - 1];
-
       setClustersList(items);
       if (items.length < pageQtyItems) {
         let emptyRows = pageQtyItems - items.length;
@@ -95,93 +97,102 @@ const Clustersoverview = (props: Props) => {
       }
     }
   };
-
-  return (
-    <>
-      <div className={classes.clustersOverviewContainer}>
-        <div
-          id="tableContainer"
-          className={classes.clustersOverviewTableContainer}
-          ref={tableContainerRef}
-        >
-          <table
-            ref={tableRef}
-            id="table"
-            className={classes.clustersOverviewTable}
+  if (isValidPage) {
+    return (
+      <>
+        <div className={classes.clustersOverviewContainer}>
+          <div
+            id="tableContainer"
+            className={classes.clustersOverviewTableContainer}
+            ref={tableContainerRef}
           >
-            <thead>
-              <tr>
-                <th>
-                  <div className={classes.tableCheckboxAllIconContainer}>
-                    <label className={classes.tableCheckboxControlAll}>
-                      <input
-                        className={classes.tableCheckboxAll}
-                        onChange={(e) => changeCheckbox(e.target.checked)}
-                        type="checkbox"
-                        name="checkbox"
-                        checked={checked}
-                      />
-                    </label>
-                  </div>
-                </th>
-                <th>
-                  <div className={classes.tableIconCol}>
-                    <div className={classes.actionsIconAllDisabled}></div>
-                  </div>
-                </th>
-                <th className={classes.responsiveTh}>
-                  <div className={classes.tableHeaderTitle}>clusters</div>
-                </th>
-                <th>
-                  <div className={classes.tableHeaderTitle}>provider</div>
-                </th>
-                <th>
-                  <div className={classes.tableHeaderTitle}>flavor</div>
-                </th>
-                <th>
-                  <div className={classes.tableHeaderTitle}>k8s version</div>
-                </th>
-                <th>
-                  <div className={classes.tableHeaderTitle}>cluster group</div>
-                </th>
-                <th>
-                  <div className={classes.tableHeaderTitle}>machines</div>
-                </th>
-                <th>
-                  <div className={classes.tableHeaderTitle}>age</div>
-                </th>
-                <th>
-                  <div className={classes.tableHeaderTitle}>status</div>
-                </th>
-              </tr>
-            </thead>
+            <table
+              ref={tableRef}
+              id="table"
+              className={classes.clustersOverviewTable}
+            >
+              <thead>
+                <tr>
+                  <th>
+                    <div className={classes.tableCheckboxAllIconContainer}>
+                      <label className={classes.tableCheckboxControlAll}>
+                        <input
+                          className={classes.tableCheckboxAll}
+                          onChange={(e) => changeCheckbox(e.target.checked)}
+                          type="checkbox"
+                          name="checkbox"
+                          checked={checked}
+                        />
+                      </label>
+                    </div>
+                  </th>
+                  <th>
+                    <div className={classes.tableIconCol}>
+                      <div className={classes.actionsIconAllDisabled}></div>
+                    </div>
+                  </th>
+                  <th className={classes.responsiveTh}>
+                    <div className={classes.tableHeaderTitle}>clusters</div>
+                  </th>
+                  <th>
+                    <div className={classes.tableHeaderTitle}>provider</div>
+                  </th>
+                  <th>
+                    <div className={classes.tableHeaderTitle}>flavor</div>
+                  </th>
+                  <th>
+                    <div className={classes.tableHeaderTitle}>k8s version</div>
+                  </th>
+                  <th>
+                    <div className={classes.tableHeaderTitle}>
+                      cluster group
+                    </div>
+                  </th>
+                  <th>
+                    <div className={classes.tableHeaderTitle}>machines</div>
+                  </th>
+                  <th>
+                    <div className={classes.tableHeaderTitle}>age</div>
+                  </th>
+                  <th>
+                    <div className={classes.tableHeaderTitle}>status</div>
+                  </th>
+                </tr>
+              </thead>
 
-            <tbody>
-              {clustersList?.map((r, index) => {
-                if (r.name != "") {
-                  return (
-                    <Clustersoverviewrow
-                      key={index + 1}
-                      cluster={r}
-                      disabled={false}
-                    />
-                  );
-                } else {
-                  return <Clustersoverviewemptyrow key={index + 1} />;
-                }
-              })}
-            </tbody>
-          </table>
-          <Clusteroverviewfooter
-            ref={pageFooterRef}
-            total={props.clusters?.length || 0}
-            currentPage={pageNumber}
-            qtdPages={qtyPages}
-          />
+              <tbody>
+                {clustersList?.map((r, index) => {
+                  if (r.name != "") {
+                    return (
+                      <Clustersoverviewrow
+                        key={index + 1}
+                        cluster={r}
+                        disabled={false}
+                      />
+                    );
+                  } else {
+                    return <Clustersoverviewemptyrow key={index + 1} />;
+                  }
+                })}
+              </tbody>
+            </table>
+            <Clusteroverviewfooter
+              ref={pageFooterRef}
+              total={props.clusters?.length || 0}
+              currentPage={pageNumber}
+              qtdPages={qtyPages}
+            />
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Page404message />
+      </>
+    );
+  }
 };
 
 export default Clustersoverview;
--- gui/components/page404/page404message.tsx
+++ gui/components/page404/page404message.tsx
@@ -5,45 +5,49 @@ import Link from "next/link";
 type Props = {};
 
 const Page404message = (props: Props) => {
-    let page404MainTextLine1UpperCase = [classes.page404MainTextLine1, "upperCase"].join(" ");
-    let page404MainTextLine2UpperCase = [classes.page404MainTextLine2, "upperCase"].join(" ");
-    let page404SecondaryTextLine1UpperCase = [classes.page404SecondaryTextLine1, "upperCase"].join(" ");
-    let page404SecondaryTextLine2UpperCase = [classes.page404SecondaryTextLine2, "upperCase"].join(" ");
-    return (
-        <>
+  let page404MainTextLine1UpperCase = [
+    classes.page404MainTextLine1,
+    "upperCase",
+  ].join(" ");
+  let page404MainTextLine2UpperCase = [
+    classes.page404MainTextLine2,
+    "upperCase",
+  ].join(" ");
+  let page404SecondaryTextLine1UpperCase = [
+    classes.page404SecondaryTextLine1,
+    "upperCase",
+  ].join(" ");
+  let page404SecondaryTextLine2UpperCase = [
+    classes.page404SecondaryTextLine2,
+    "upperCase",
+  ].join(" ");
+  return (
+    <>
+      <div className={classes.page404ContainerMessage}>
+        <div className={classes.page404messageContainer}>
+          <div className={classes.page404MonitorMessage}></div>
 
-<div className={classes.page404ContainerMessage}>
+          <div className={page404MainTextLine1UpperCase}>
+            it seems that one of our
+          </div>
+          <div className={page404MainTextLine2UpperCase}>
+            trainees screwed up again...
+          </div>
 
-{/*<div className={classes.page404logoContainer}>
-
-    <div className={classes.page404logo}></div>
-
-    </div>*/}
-
-<div className={classes.page404messageContainer}>
-
-    <div className={classes.page404MonitorMessage}></div>
-
-    <div className={page404MainTextLine1UpperCase}>
-        it seems that one of our
-    </div>
-    <div className={page404MainTextLine2UpperCase}>
-        trainees screwed up again...
-    </div>
-
-    <div className={page404SecondaryTextLine1UpperCase}>
-        you can go to the <a>home page</a> while
-    </div>
-    <div className={page404SecondaryTextLine2UpperCase}>
-        we look for someone to blame
-    </div>
-
-</div>
-
-</div>
-
-        </>
-    );
-}
+          <div className={page404SecondaryTextLine1UpperCase}>
+            you can go to the{" "}
+            <Link href="/">
+              <a>home page</a>
+            </Link>{" "}
+            while
+          </div>
+          <div className={page404SecondaryTextLine2UpperCase}>
+            we look for someone to blame
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Page404message;
